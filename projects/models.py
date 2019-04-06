56aa6ee3ee990309cfa4250df48e1dd897139ee6
Dinara Aleskarova <aleskarova@gmail.com>
2019-04-06 23:57:56 +0400

    Creating migrations for 'projects' app

--- projects/models.py
+++ projects/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    descritpion = models.TextField()
+    description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.FilePathField(path='/img')
